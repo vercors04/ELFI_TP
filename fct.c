1c47d56a856291cdac367866965659016771f2a2
Amyne <amynek19@gmail.com>
2026-01-15 09:45:48 +0100

    Correction formule h1 et h2 + boucle calcul des coordonnees

--- fct.c
+++ fct.c
@@ -2,23 +2,26 @@
 
 void file_create (int a, int b, int c, int d, int n1, int n2, int t){
 	FILE *f = fopen("fichier.txt", "w"); 
+
+  //Nombre de noeuds
 	fprintf(f, "%d\n", n1*n2);
 	
-	//calcul des coordonées
-	double h1=(b-a)/n1;
-	double h2 = (d-c)/n2;
+	//Calcul des coordonées
+	double h1 = (double) (b-a)/(n1-1);
+	double h2 = (double) (d-c)/(n2-1);
+
 	for (int j=0 ; j<n2 ; j++){
 		double y=c+j*h2;
 		for (int i=0 ; i<n1 ; i++){
-			fprintf(f, "%lf%lf\n",a+j*h1,y);
+			fprintf(f, "%lf %lf\n",a+i*h1,y);
 		}
-	}	
+	}
 	
-	//mtpq
-	if (t==1) fprintf(f, "%d%d%d%d\n",(n1-1)*(n2-1),t,4,0);
-	if (t==2) fprintf(f, "%d%d%d%d\n",((n1-1)*(n2-1))*2,t,3,0);
+	//m t p q
+	if (t==1) fprintf(f, "%d %d %d %d\n",(n1-1)*(n2-1),t,4,0); //Quadrangles
+	if (t==2) fprintf(f, "%d %d %d %d\n",((n1-1)*(n2-1))*2,t,3,0); //Triangles
 	
-	//calcul des numéros globaux
+	//Calcul des numéros globaux
 	if (t==2) {
 		for(int j=0; j<n2 ;j++){
 			for (int i=1; i<n1; i++) {
@@ -27,8 +30,8 @@ void file_create (int a, int b, int c, int d, int n1, int n2, int t){
 				fprintf (f, "%d%d%d\n",indice+n1,indice+1,indice+1+n1);
 			}
 		}
-	}		
-			
+	}
+
 	if (t==1) {
 		for(int j=0; j<n2 ;j++){
 			for (int i=1; i<n1; i++) {
--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include "header.h"
 
 int main() {
-    int a, b, c, d, n1, n2,t;
+  int a, b, c, d, n1, n2,t;
 
     //ouverture du fichier
-    FILE *f = fopen("fichier_imput_test.txt", "r");
-	fscanf(f, "%d %d %d %d %d %d", &a, &b, &c, &d, &n1, &n2,&t); //lecture du fichier et on met dans les var
-    printf("test : %d %d",a,n1);
-    file_create(a,b,c,d,n1,n2,t);
+
+  FILE *f = fopen("fichier_imput_test.txt", "r");
+  fscanf(f, "%d %d %d %d %d %d %d", &a, &b, &c, &d, &n1, &n2,&t); //lecture du fichier et on met dans les var
+  
+  printf("Domaine : [%d,%d]x[%d,%d]\n",a,b,c,d);
+  printf("Nombres de points n1 = %d\n",n1);
+  printf("Nombres de points n2 = %d\n",n2);
+  if (1==t) printf("Type : Quadrangles\n");
+  else if (2==t) printf("Type : Triangles\n");
+  file_create(a,b,c,d,n1,n2,t);
 
 	
 }
