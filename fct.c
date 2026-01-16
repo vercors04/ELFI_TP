d00f86290a2eebd7a4e028f7c504a1149acb8539
Vercors04 <xavkrs@gmail.com>
2026-01-16 10:07:16 +0100

    a voir

--- fct.c
+++ fct.c
@@ -36,7 +36,7 @@ void file_create (int a, int b, int c, int d, int n1, int n2, int t){
 		for(int j=0; j<n2 ;j++){
 			for (int i=1; i<n1; i++) {
 					int indice = i+ j * n1;
-					fprintf (f, "%d%d%d%d\n",indice+1,indice+1+n1,indice+n1,indice);
+					fprintf (f, "%d %d %d %d\n",indice+1,indice+1+n1,indice+n1,indice);
 				}
 			}
 		}
