1abc21aa041c740b30274589791698a0df43ad47
Amyne <amynek19@gmail.com>
2026-01-20 17:20:37 +0100

    Ajout du choix du moyen d'entree des donnees - modification du nom input.txt

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
-# A savoir :
-___
+# A savoir
+
 - les bornes a,b,c,d définissent le domaine
 - le nombre de points n1 et n2 sur les coté 1 et 2
 - le type t des éléments à construire ( 1=quadrangle, 2= triangle)
 - les 5 numéros de références : celui associé au domaine et celui associé à chacun des 4 côtés
 
+# Problèmes à résoudre
 
+# Modifications
 
-# Problèmes à résoudre :
-___
-
-
-
-# Modifications :
-___
 **16/01** - résolution de l'indice j, quelques mise en forme, mettre a,b,c,d en double
--- main.c
+++ main.c
@@ -1,20 +1,53 @@
 #include "header.h"
 
 int main() {
-  int  n1, n2,t;
+  int  n1, n2, t;
   double a, b, c, d;
 
-    //ouverture du fichier
+  // Choix du mode d'entree des donnees
+  printf("1. Lecture des données dans le fichier : input.txt \n2. Entrée manuelle des données \n");
+  int mode_saisie = 2;
+  scanf("%d",&mode_saisie);
+  mode_saisie = mode_saisie - 1;
 
-  FILE *f = fopen("fichier_imput_test.txt", "r");
-  fscanf(f, "%lf %lf %lf %lf %d %d %d", &a, &b, &c, &d, &n1, &n2,&t); //lecture du fichier et on met dans les var
-  
-  printf("Domaine : [%lf,%lf]x[%lf,%lf]\n\n",a,b,c,d);
-  printf("Nombres de points n1 = %d\n\n",n1);
-  printf("Nombres de points n2 = %d\n\n",n2);
-  if (t==1) printf("Type : Quadrangles\n\n");
-  else if (t==2) printf("Type : Triangles\n\n");
+  if (!mode_saisie) {
+
+    // Ouverture du fichier
+    FILE *donnees = fopen("input.txt", "r");
+    // Test du bon deroulement de l'ouverture
+    if (donnees==NULL) {
+      printf("ERREUR : Le fichier n'a pas pu etre ouvert.\n");
+      return 1;
+    }
+    // Lecture du fichier et on met dans les var
+    fscanf(donnees, "%lf %lf %lf %lf %d %d %d", &a, &b, &c, &d, &n1, &n2, &t);
+    // Fermeture du fichier
+    fclose(donnees);
+  }
+  else {
+
+    printf("Inserez les donnees :\n\nDomaine [a,b]x[c,d] (Entrez les valeures de a b c d) :\n");
+    scanf("%lf %lf %lf %lf", &a, &b, &c, &d);
+    printf("\nNombres de points n1 et n2 :\n");
+    scanf("%d %d", &n1, &n2);
+    printf("\nType des elements a construire (1. Quadrangles - 2. Triangles) : \n");
+    scanf("%d", &t);
+  }
+
+
+  // Affichage des donnees
+  printf("Domaine : [%lf,%lf]x[%lf,%lf]\n",a,b,c,d);
+  printf("Nombres de points n1 = %d\n",n1);
+  printf("Nombres de points n2 = %d\n",n2);
+  if (t==1) printf("Type : Quadrangles\n");
+  else if (t==2) printf("Type : Triangles\n");
+  else {
+    printf("ERREUR : pas de type d'elements correctement initialise");
+    return 1;
+  }
+
   file_create(a,b,c,d,n1,n2,t);
-//test3
-	
+
+
+  return 0;
 }
