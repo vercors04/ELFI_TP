8d02060e847a6500dfffef396df7a1c85228e736
Vercors04 <xavkrs@gmail.com>
2026-03-04 16:36:42 +0100

    a

--- ElementaireB/intElem.c
+++ ElementaireB/intElem.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+#include "../Utilitaire/utilitaires.h"
 
 
-void intElem (int t, int nbneel, float** matelm, float* vecelem){
+void intElem (int t, int nbneel, float** coordElem, float** matelm, float* vecelem){
+    float** cofvar = alloctab (2, 2);
+    float* x = allocvec (2);
+    int q = q_associate(t);
     
+
+
+
+    transFK(nbneel, coordElem, )
 
 
 }
