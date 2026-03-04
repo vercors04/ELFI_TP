19f86441dafbc9e2027a9542e99926d9be090d5a
Vercors04 <xavkrs@gmail.com>
2026-03-04 16:32:47 +0100

    fin 4/03

--- ElementaireB/adwdw.c
+++ ElementaireB/adwdw.c
@@ -1,4 +1,7 @@
-void ADWDW (int nbneel, float** dpfctbas, float** cofvar, float eltdif, float** matelm){
+#include <stdio.h>
+
+
+void ADWDW (int nbneel, float** dpfctbas, float eltdif, float** cofvar, float** matelm){
   float som1, som2, som3, som4;
   for (int i=0; i<nbneel; i++) {
     for (int j=0; j<nbneel; j++) {
@@ -14,8 +17,8 @@ void ADWDW (int nbneel, float** dpfctbas, float** cofvar, float eltdif, float** 
 }
 
 /*
- * cofvar : matrice 2x2 de la forme suivante :  A00 A01
- *                                              A10 A11
+ * cofvar : matrice 2x2 de la forme suivante :  A11 A12
+ *                                              A21 A22
  *
  * dpfctbas : matrice qx2 de la forme suivante :  dw1/dx1 ; dw1/dx2
  *                                                dw2/dx1 ; dw2/dx2
@@ -28,7 +31,7 @@ void ADWDW (int nbneel, float** dpfctbas, float** cofvar, float eltdif, float** 
 /*
 creation d'une matrice avec pour chaque ligne correspondant à un noeud les dérivée partielle en ce 
 noeud * la colonne associé à alpha (1 ou 2) de l'inverse de la matrice jacobienne de la transformée Fk 
-void pder_WI (int nbneel, float** der_fctbas, float** invjacob, float** res){
+void pder_WI (int nbneel, float** der_fctbas, float** invjacob, float** dpfctbas){
   for (int i=0 ; i<nbneel; i++) {
       res [i][0]=der_fctbas[i][0] * invjacob [0][0] + der_fctbas[i][1] * invjacob [1][0];
       res [i][1]=der_fctbas[i][0] * invjacob [0][1] + der_fctbas[i][1] * invjacob [1][1];
