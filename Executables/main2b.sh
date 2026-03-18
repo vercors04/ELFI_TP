8593732e3e5c4c4c1a62a1cef07adccdec33e8b2
Vercors04 <xavkrs@gmail.com>
2026-03-18 15:06:43 +0100

    ..

--- Executables/main2b.sh
+++ Executables/main2b.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc ../ElementaireB/intAret.c ../ElementaireB/main2b.c ../Utilitaire/alloctab.c ../Utilitaire/freetab.c ../Utilitaire/freevec.c ../Maillage/fct_maillage.c ../ElementaireA/fct_elementairesa.c ../ElementaireB/intElem.c ../ElementaireB/cal1Elem.c ../ElementaireB/impcalel.c ../Utilitaire/allocvec.c ../ElementaireB/fct_def_pb.c ../ElementaireB/w.c ../ElementaireB/ww.c ../ElementaireB/adwdw.c -o main2b.exe -lm
+gcc ../ElementaireB/intAret.c ../ElementaireB/main2b.c ../Utilitaire/alloctab.c ../Utilitaire/freetab.c ../Utilitaire/freevec.c ../Maillage/lecfima.c ../ElementaireA/fct_elementairesa.c ../ElementaireB/intElem.c ../ElementaireB/cal1Elem.c ../ElementaireB/impcalel.c ../Utilitaire/allocvec.c ../ElementaireB/fct_def_pb.c ../ElementaireB/w.c ../ElementaireB/ww.c ../ElementaireB/adwdw.c -o main2b.exe -lm
