8f428e969f51f7b8b30be845975fdfcc3be44599
Amyne <amynek19@gmail.com>
2026-05-07 22:32:34 +0200

    Création des graphes

--- Resultats/plot.c
+++ Resultats/plot.c
@@ -13,14 +13,14 @@ int main() {
         } 
 
     int nudom = impfch_Aff/100;
-    int nucas = nudom%10;
+    int nucas = (impfch_Aff / 10) % 10;
     int typel = impfch_Aff%10;
 
     /*popen lance gnuplot -persist (persist pour pas que le graph se ferme quand on ferme le fichier)
     et retourne un FILE* */
     FILE* plot = popen("gnuplot -persist", "w");
 
-    fprintf(plot, "set title 'domaine %d, cas %d, typel %d'\n", nudom, nucas, typel);
+    fprintf(plot, "set title 'Erreur obtenue pour le domaine %d, le cas %d et le type d''élément %d'\n", nudom, nucas, typel);
     fprintf(plot, "set xlabel 'h'\n");
     fprintf(plot, "set ylabel 'Erreur relative'\n");
     fprintf(plot, "set xrange [*:*] reverse\n");
